9ab1771457b6b17714f95283aa6048b9ea4033b6
Evans Oduor <oduorevans9@gmail.com>
2022-10-18 19:12:21 +0300

    Correcting the errors of the markdown field

--- routes/articles.js
+++ routes/articles.js
@@ -4,14 +4,14 @@ const router=express.Router()
 const Article=require('./../models/article')
 
 router.get('/new', (req,res)=>{
-    res.render('articles/new')
+    res.render('articles/new', {article:new Article()})
 })
 router.get('/:id',(req,res,)=>{
-
+res.send(req.params.id)
 })
 
 router.post('/', async(req,res)=>{
-const article=new Article({
+let article=new Article({
    title: req.body.title,
    description: req.body.description,
    markdown: req.body.markdown
